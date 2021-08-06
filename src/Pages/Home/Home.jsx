1c9665d6ebf86017f28ed1befcf6e443f5899e82
Rahul Vedi <rahul.v7800@gmail.com>
2021-08-06 12:17:56 +0530

    make a new branch

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,12 @@
-import React,{useState} from 'react'
+import React from 'react'
 import {Navbar} from '../../Components/default'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 const Home = () => {
-    const [Count, setCount] = useState(0)
-    function handleClick(){
-        setCount(Count+1)
-    }
     return (
         <>
             <Navbar/>
-            <Link to='/about'>go to about</Link>
-            <p onClick={handleClick}>{Count}</p>
+           <p>hi rahul</p>
         </>
     )
 }
-
 export default Home
