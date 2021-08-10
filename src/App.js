4d61f22a3df333442e1e7bd5ce82a370c2018dfd
Rahul Vedi <rahul.v7800@gmail.com>
2021-08-10 11:01:56 +0530

    fix some error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router,Switch,Route} from  'react-router-dom'
 import Home from './Pages/Home/Home'
-import About from './Pages/About/About'
+// import About from './Pages/About/About'
 import Login from './Pages/Auth/Login/Login'
 import RegisterAsStudent from './Pages/Auth/Register/ResgisterStudent'
 function App() {
