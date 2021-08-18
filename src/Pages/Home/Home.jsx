cef7160dfad48adf9deb1297fe9cebe015cad606
Rahul Vedi <rahul.v7800@gmail.com>
2021-08-18 13:59:04 +0530

    added feature section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import Sec3 from './Sections/Sec3/Sec3.jsx'
 // import Sec4 from './Sections/Sec4/Sec4'
 import Sec5 from './Sections/Sec5/Sec5'
 import Sec6 from './Sections/Sec6/Sec6'
+import Sec7 from './Sections/Sec7/Sec7'
 const Home = () => {
     return (
         <>
@@ -16,6 +17,7 @@ const Home = () => {
             {/* <Sec4/> */}
             <Sec5/>
             <Sec6/>
+            <Sec7/>
         </>
     )
 }
---
+++ src/Pages/Home/Sections/Sec7/Sec7.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Section, Title, Content, Card, Icon, Text, SubText,Button } from './Sec7Styles'
+import { BsCameraVideo } from 'react-icons/bs'
+import { GoRepo } from 'react-icons/go'
+import { FaRegSmileBeam } from 'react-icons/fa'
+import { HiUsers } from 'react-icons/hi'
+import { AiOutlineHighlight } from 'react-icons/ai'
+const Sec7 = () => {
+    return (
+        <Section>
+            <Title>Experience the Best
+                @ Tuitionpad.com </Title>
+            <Content>
+                <Card>
+                    <Icon><GoRepo /></Icon>
+                    <Text>Ask Unlimited Doubts</Text>
+                    <SubText>Don't sit calm until you get clear concepts. You can always raise hand to ask your doubts.</SubText>
+                </Card>
+                <Card>
+                    <Icon><GoRepo/></Icon>
+                    <Text>Weekly Reports</Text>
+                    <SubText>Outcome based methodology, we share your child's learning evaluation reports every week..</SubText>
+                </Card>
+                <Card>
+                    <Icon><BsCameraVideo /></Icon>
+                    <Text>Revise Your Live Class</Text>
+                    <SubText>Revise your real face to face class whenever you do subject revision.</SubText>
+                </Card>
+                <Card>
+                    <Icon><FaRegSmileBeam /></Icon>
+                    <Text>Support Manager</Text>
+                    <SubText>Dedicated student support manager for parents for care, guide and best experience</SubText>
+                </Card>
+                <Card>
+                    <Icon><HiUsers /></Icon>
+                    <Text>Face to face Live Class</Text>
+                    <SubText>Real-time interaction,
+                        5 times more engagement than video courses</SubText>
+                </Card>
+                <Card>
+                    <Icon><AiOutlineHighlight /></Icon>
+                    <Text> Practice Test</Text>
+                    <SubText>Regular practice tests to evaluate subjects and for better exam preparation</SubText>
+                </Card>
+            </Content>
+            <Button>Book Trail Classes</Button>
+        </Section>
+    )
+}
+export default Sec7
