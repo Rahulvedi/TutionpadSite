ad9011cc1a53f2d599613b8f2aa0064eb6483b6b
Vijendra <vijendrakumawat8710@gmail.com>
2021-08-31 19:09:11 +0530

    Notification component

--- src/Pages/DashBoard/DashBoardUI/Tabs/Profile/Tabs/Notifications/Notifications.jsx
+++ src/Pages/DashBoard/DashBoardUI/Tabs/Profile/Tabs/Notifications/Notifications.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
-
+import { Details, Group, Heading, Input, Label, UpdateButton } from './NotificationsStyle';
 const Notifications = () => {
     return (
-        <div>
-            Notifications
-        </div>
+        <>   
+            <Heading>Notifications</Heading>
+            <hr/>
+ 
+            <Details>   
+            <h4 className="mb-5">Email Notifications</h4>                      
+                <Group className="mb-3" >
+                    <Input type="checkbox" />
+                    <Label> <b>General Reminders</b><br/>
+                    Notifications about lessons, tutor messages, and payments.
+                    </Label>
+                </Group>
+                <Group className="mb-3">
+                    <Input type="checkbox" />
+                    <Label> <b>Updates, tips and offerss</b><br/>
+                    Stay connected with product updates, helpful tips and special offers.
+                    </Label>
+                </Group>
+                <Group className="mb-3">
+                    <Input type="checkbox" />
+                    <Label> <b>Tuitionpad Blog</b><br/>
+                    Occasional newsletter with the latest posts.
+                    </Label>
+                </Group>
+                <Group className="mb-3">
+                    <Input type="checkbox" />
+                    <Label> <b>Q  and A section</b><br/>
+                    Receive tutors’ replies to your questions.
+                    </Label>
+                </Group>                        
+                <Group className="mb-3">
+                    <Input type="checkbox" />
+                    <Label> <b>Lessons and messages</b><br/>
+                    SMS alerts about tutors' first responses to your requests and trial lesson reminders.
+                    </Label>
+                </Group>                        
+                <UpdateButton>Save Settings</UpdateButton>
+            </Details>
+        </>
     )
 }
 
---
+++ src/Pages/DashBoard/DashBoardUI/Tabs/Profile/Tabs/Notifications/NotificationsStyle.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import {Form} from 'react-bootstrap'
+export const Heading = styled.h3``;
+
+export const Details=styled(Form)`
+padding: 2rem;
+`
+export const Group=styled(Form.Group)`
+padding:.65rem;
+border-radius:10px;
+background:#C8FFFC;
+
+`
+export const Label=styled.label`
+font-size:1rem;
+`
+export const Input=styled.input`
+ &:checked{
+    background:#6BFCF3;
+    color:#007770;
+    font-size:1.5rem;
+    padding:.5rem;
+    border-radius:20rem;
+  }
+
+`
+
+export const UpdateButton=styled.button`
+border: none;
+outline: none;
+padding: .7rem 1.3rem;
+background-color: #007770;
+border-radius: 10px;
+color: white;
+font-size: 1rem;
+font-weight: 500;
+display: block;
+margin-top: 2rem;
+`
